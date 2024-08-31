ef3e847d3a6554934324bbb44ab20b156039fd23
Zhenzhong Xu <flyfree@gmail.com>
2024-08-31 15:15:32 -0700

    add ssh config to dev container

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-nohup bash -c 'minikube start &' > minikube.log 2>&1
+pwd
+# nohup bash -c 'init_python_environment.sh' > init_python_environment.log 2>&1
+# nohup bash -c 'minikube start &' > minikube.log 2>&1
